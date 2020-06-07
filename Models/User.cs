908e66ceaedd96bb2ecc72f8399e65ca1cea9b0e
R2DEV0 <r2devo@gmail.com>
2020-06-07 07:44:43 -0700

    updated validations

--- Models/User.cs
+++ Models/User.cs
@@ -4,15 +4,24 @@ namespace Dojo_survey.Models
     public class Person
     {
         [Required(ErrorMessage="This is required")]
-        [MinLength(2, ErrorMessage="Name must be at least 2 characters long")]
+        [MinLength(2, ErrorMessage="Name must be at least 2 characters")]
         [Display(Name = "Name: ")]
         public string Name {get; set;}
+
+
+
         [Required(ErrorMessage="This is required")]
         [Display(Name = "Location: ")]
         public string Location{get; set;}
+
+
+
         [Required(ErrorMessage="This is required")]
         [Display(Name = "Language: ")]
         public string Language{get; set;}
+
+
+        [MinLength(20, ErrorMessage="Comment must be at least 20 characters")]
         [Display(Name = "Comment: ")]
         public string Comment{get; set;}
     }
